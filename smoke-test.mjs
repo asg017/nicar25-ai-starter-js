e05087aa973d3afa435d90daace5d567b4a27424
Alex Garcia <alexsebastian.garcia@gmail.com>
2025-02-12 23:14:07 -0800

    init

---
+++ smoke-test.mjs
@@ -0,0 +1,10 @@
+//import * as x from "@ai-sdk/openai";
+import * as x1 from "@huggingface/transformers";
+import * as x2 from "ai";
+import * as x3 from "ollama";
+import * as x4 from "zod";
+
+import * as x5 from "better-sqlite3";
+import * as x6 from "sqlite-vec";
+
+console.log("NICAR25 Everything works!");
