b574f39e76878344904502ae2dd6bd65368badf7
Alex Garcia <alexsebastian.garcia@gmail.com>
2025-03-07 13:57:54 -0600

    udpates

--- embeddings-nicar.mjs
+++ embeddings-nicar.mjs
@@ -139,29 +139,7 @@ async function searchSessions(path, query) {
   }
 }
 async function searchSpeakers(path, query) {
-  console.log(`Searching for NICAR speakers related to: ${query}...`);
-  const db = new Database(path);
-  sqliteVec.load(db);
-  const result = await ollama.embed({
-    model: "all-minilm",
-    input: query
-  });
-  const queryEmbedding = result.embeddings[0];
-  const knnQuery = db.prepare(`
-      SELECT 
-        rowid, 
-        distance,
-        speakers.bio
-      FROM vec_speakers 
-      LEFT JOIN speakers ON speakers.speaker_id = vec_speakers.rowid
-      WHERE bio_embedding MATCH ? 
-        AND k = 10;
-    `);
-  
-  console.log(query);
-  for(const {rowid, distance, bio} of knnQuery.all(JSON.stringify(queryEmbedding))) {
-    console.log(rowid, distance, bio);
-  }
+  // TODO try yourself!
 }
 
 async function main() {
